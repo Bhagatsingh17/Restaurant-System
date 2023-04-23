e6eac7057ad0ab7228294cf9e8e65cd4e7c1f6c2
bhagat <bhagatyadav610@gmail.com>
2023-04-23 14:52:43 +0530

    first commit

---
+++ src/main/java/com/geekster/Restaurant/Model/repository/RestaurantDao.java
@@ -0,0 +1,41 @@
+package com.geekster.Restaurant.Model.repository;
+
+import com.geekster.Restaurant.Model.model.Restaurant;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RestaurantDao {
+    private List<Restaurant> restaurantList;
+
+    public RestaurantDao() {
+        restaurantList=new ArrayList<>();
+    }
+
+    public boolean save(Restaurant restaurant) {
+        restaurantList.add(restaurant);
+        return true;
+    }
+
+    public List<Restaurant> getAllRestaurantFromData() {
+        return restaurantList;
+    }
+
+    public boolean update(int id, Restaurant restaurant) {
+        for(Restaurant res:restaurantList){
+            if(res.getRestaurantId()==id){
+                remove(res);
+                save(restaurant);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remove(Restaurant res) {
+        restaurantList.remove(res);
+        return true;
+    }
+}
